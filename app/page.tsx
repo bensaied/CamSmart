24f2aba6bea3a53404d80716e9bd89de5c4ca108
Oussama_Ben_Saied <saayeed@outlook.fr>
2024-04-08 13:51:34 +0200

    Install App dependencies modules

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Separator } from "@/components/ui/separator";
+import { Slider } from "@/components/ui/slider";
+import {
+  Camera,
+  MoonIcon,
+  Disc2Icon,
+  SunIcon,
+  Video,
+  Volume2,
+  ImageIcon,
+  CctvIcon,
+} from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
