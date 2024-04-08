7521ac295686bbe6dd8b998b1fd61c98a9693c0c
Oussama_Ben_Saied <saayeed@outlook.fr>
2024-04-08 14:12:59 +0200

    Feat: Add formatDate function

--- app/page.tsx
+++ app/page.tsx
@@ -64,3 +64,18 @@ function resizeCanvas(
     canvas.height = videoHeight;
   }
 }
+function formatDate(d: Date) {
+  const formattedDate =
+    [
+      (d.getMonth() + 1).toString().padStart(2, "0"),
+      d.getDate().toString().padStart(2, "0"),
+      d.getFullYear(),
+    ].join("-") +
+    " " +
+    [
+      d.getHours().toString().padStart(2, "0"),
+      d.getMinutes().toString().padStart(2, "0"),
+      d.getSeconds().toString().padStart(2, "0"),
+    ].join("-");
+  return formattedDate;
+}
