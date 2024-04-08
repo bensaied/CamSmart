f5e0b31ff2ca64b6ad605f5afb82c011ad396a91
Oussama_Ben_Saied <saayeed@outlook.fr>
2024-04-08 14:12:18 +0200

    Feat: Add resizeCanvas function

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 import { useTheme } from "next-themes";
-
 import { Button } from "@/components/ui/button";
 import {
   Popover,
@@ -20,8 +19,48 @@ import {
   ImageIcon,
   CctvIcon,
 } from "lucide-react";
+import Webcam from "react-webcam";
 import Image from "next/image";
 
-export default function Home() {
-  return <h1> CamSmart </h1>;
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import "@tensorflow/tfjs-backend-cpu";
+import "@tensorflow/tfjs-backend-webgl";
+import { DetectedObject, ObjectDetection } from "@tensorflow-models/coco-ssd";
+
+import { ModeToggle } from "@/components/theme-toggle";
+
+type Props = {};
+
+let interval: any = null;
+let stopTimeout: any = null;
+const HomePage = (props: Props) => {
+  // Theme
+  const { setTheme } = useTheme();
+  const webcamRef = useRef<Webcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  // States
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [autoRecordEnabled, setAutoRecordEnabled] = useState<boolean>(false);
+  const [volume, setVolume] = useState(0.8);
+  const [model, setModel] = useState<ObjectDetection>();
+  const [loading, setLoading] = useState(false);
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+
+  return <h1>CamSmart</h1>;
+};
+
+// Functions resizeCanvas FormatData base64toBlob
+function resizeCanvas(
+  canvasRef: React.RefObject<HTMLCanvasElement>,
+  webcamRef: React.RefObject<Webcam>
+) {
+  const canvas = canvasRef.current;
+  const video = webcamRef.current?.video;
+
+  if (canvas && video) {
+    const { videoWidth, videoHeight } = video;
+    canvas.width = videoWidth;
+    canvas.height = videoHeight;
+  }
 }
